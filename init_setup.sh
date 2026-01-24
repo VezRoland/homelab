2fcb0badd1008f920d70daad6632078063029814
VezRoland <devrjs4nyhev2cwfa4@rolandvezsenyi.com>
2026-01-24 12:43:36 +0100

    Adds minor config changes

--- init_setup.sh
+++ init_setup.sh
@@ -1,5 +1,5 @@
 docker network create --driver bridge --attachable traefik
-docker network create --driver macvlan --subnet=192.168.0.0/24 --gateway=192.168.0.1 -o parent=ens18 adguard
+docker network create --driver macvlan --subnet=10.10.10.0/24 --gateway=10.10.10.1 -o parent=$(ip route show default | awk '/default/ {print $5}') adguard
 
 sudo ln -s ${PWD}/adguardhome/adguard-shim.service /etc/systemd/system/adguard-shim.service
 sudo systemctl daemon-reload
